66abbd30563a1dc4daba8244b9331248e2249959
John Tambi <jon.tambi@gmail.com>
2019-11-07 18:07:48 -0500

    Add VPC - SUBNET - GATEWAY

--- gitlab.tf
+++ gitlab.tf
@@ -18,18 +18,90 @@ terraform {
 
 provider "aws" {
     region = "us-east-2"
-  
 }
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# DEPLOY VPC
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+resource "aws_vpc" "vpc_gitlab" {
+    cidr_block = "172.16.0.0/16"
+    enable_dns_hostnames = true
+    enable_dns_support = true
+    tags = {
+        Name = "VPC GitLab Instance"
+    }
+}
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# DEPLOY INTERNET GATEWAY
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+resource "aws_internet_gateway" "gateway_gitlab" {
+    vpc_id = aws_vpc.vpc_gitlab.id
+    tags = {
+        Name = "Gateway GitLab"
+    }
 
+}
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# DEPLOY SUBNET
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+resource "aws_subnet" "subnet_pub_east_1a" {
+    cidr_block = "172.16.10.0/24"
+    vpc_id = aws_vpc.vpc_gitlab.id
+    availability_zone = "us-east-1a"
+    tags = {
+        Name = "gitlab_public_172.16.10.0"
+    }
+}
 
+resource "aws_subnet" "subnet_priv_east_1a" {
+    cidr_block = "172.16.30.0/24"
+    vpc_id = aws_vpc.vpc_gitlab.id
+    availability_zone = "us-east-1a"
+    tags {
+        Name = "gitlab_private_172.16.30.0"
+    }
+}
+
+resource "aws_subnet" "subnet_public_east_2a" {
+    cidr_block = "172.16.20.0/24"
+    vpc_id = aws_vpc.vpc_gitlab.id
+    availability_zone = "us-east-2a"
+    tags = {
+        Name = "gitlab_public_172.16.20.0"
+    }
+}
+
+resource "aws_subnet" "subnet_priv_east_2a" {
+    cidr_block = "172.16.40.0/24"
+    vpc_id = aws_vpc.vpc_gitlab.id
+    tags = {
+        Name = "gitlab_public_172.16.40.0"
+    }
+}
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#DEPLOY ROUTE TABLE
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+resource "aws_route_table" "default_route" {
+    vpc_id = aws_vpc.vpc_gitlab.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.gateway_gitlab.id
+    }
+    tags {
+        Name = "Default Route - GitLab"
+    }
+}
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #DEPLOY EC2 GitLab INSTANCE
-#This template runs a simple "Apache Tomcat" webserver on a single EC2 Instance
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+resource "aws_instance" "gitlab" {
+    ami = ""
+    instance_type = ""
+}
 
 
 
