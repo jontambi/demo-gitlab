86b76d244d6c7fbfd7328b9babc9c992acd7ed0b
John Tambi <jon.tambi@gmail.com>
2019-11-17 22:17:11 -0500

    Configuracion GitLab

--- files/gitlab_installation.sh
+++ files/gitlab_installation.sh
@@ -1,16 +1,35 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Configuration Environment
-sudo yum install -y curl policycoreutils-python openssh-server
-sudo systemctl enable sshd
-sudo systemctl start sshd
-sudo firewall-cmd --permanent --add-service=http
-sudo firewall-cmd --permanent --add-service=https
-sudo systemctl reload firewalld
+# Instalar dependencias
 
-# Install GitLab
-curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.rpm.sh | sudo bash
+sudo yum install -y curl policycoreutils-python openssh-server cronie
+sudo lokkit -s http -s ssh
+sudo yum update -y
 
-sudo yum install -y gitlab-ee
+# Set hostname
+sudo hostnamectl set-hostname gitlab.osiris.com
+sudo echo "127.0.0.1    gitlab.osiris.com    gitlab" >> /etc/hosts
 
+# Agregar repositorio GitLab
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh | sudo bash
+sudo EXTERNAL_URL="http://gitlab.osiris.com" yum -y install gitlab-ce
 
+# SMTP Setting
+
+sudo cat >> /etc/gitlab/gitlab.rb << EOF
+gitlab_rails['smtp_enable'] = true
+gitlab_rails['smtp_address'] = "smtp.gmail.com"
+gitlab_rails['smtp_port'] = 587
+gitlab_rails['smtp_user_name'] = "jontambi.business@gmail.com"
+gitlab_rails['smtp_password'] = "j0hnc!t027"
+gitlab_rails['smtp_domain'] = "smtp.gmail.com"
+gitlab_rails['smtp_authentication'] = "login"
+gitlab_rails['smtp_enable_starttls_auto'] = true
+gitlab_rails['smtp_tls'] = false
+gitlab_rails['smtp_openssl_verify_mode'] = 'peer' # Can be: 'none', 'peer', 'client_once', 'fail_if_no_peer_cert', see http://api.rubyonrails.org/classes/ActionMailer/Base.html
+EOF
+
+sudo gitlab-ctl reconfigure
+
+# Restart server
+sudo reboot
